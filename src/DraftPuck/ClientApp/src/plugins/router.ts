10268a5f50a13a80596fa9e65c014540068c724e
tyler.roper <tyroper22@gmail.com>
2023-02-15 21:24:13 -0500

    metadata

--- src/DraftPuck/ClientApp/src/plugins/router.ts
+++ src/DraftPuck/ClientApp/src/plugins/router.ts
@@ -7,19 +7,73 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('@/views/Home.vue')
+        component: () => import('@/views/Home.vue'),
+        meta: {
+            title: "DRAFTPUCK - A live hockey drinking game.",
+            metaTags: [
+                { name: "title", content: "DRAFTPUCK - A live hockey drinking game." },
+                { name: "description", content: "Invite your friends, pick players, and give out drinks based on real-time events during live NHL games!" },
+
+                { property: "og:type", content: "website" },
+                { property: "og:url", content: "https://draftpuck.com/" },
+                { property: "og:title", content: "DRAFTPUCK - A live hockey drinking game." },
+                { property: "og:description", content: "Invite your friends, pick players, and give out drinks based on real-time events during live NHL games!" },
+                { property: "og:image", content: "/dist/assets/meta-img.png" },
+
+                { property: "twitter:card", content: "summary_large_image" },
+                { property: "twitter:url", content: "https://draftpuck.com/" },
+                { property: "twitter:title", content: "DRAFTPUCK - A live hockey drinking game." },
+                { property: "twitter:description", content: "Invite your friends, pick players, and give out drinks based on real-time events during live NHL games!" },
+                { property: "twitter:image", content: "/dist/assets/meta-img.png" }
+            ]
+        }
     },
     {
         path: '/lobby/:joinCode',
         name: 'Lobby',
         props: true,
-        component: () => import('@/views/Lobby.vue')
+        component: () => import('@/views/Lobby.vue'),
+        meta: {
+            title: `DRAFTPUCK (Lobby: {{joinCode}})`,
+            metaTags: [
+                { name: "title", content: `DRAFTPUCK (Lobby: {{joinCode}})` },
+                { property: "og:title", content: `DRAFTPUCK (Lobby: {{joinCode}})` },
+                { property: "twitter:title", content: `DRAFTPUCK (Lobby: {{joinCode}})` }
+            ]
+        }
     }
 ];
 
 const router = new Router({
     mode: 'history',
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    const metaTags: Array<object> = to.meta?.metaTags ?? [];
+    metaTags.forEach(tag => {
+        const firstKey = Object.keys(tag)[0];
+        const element = document.querySelector(`meta[${firstKey}='${tag[firstKey]}']`);
+        if (element) {
+            let str = tag.content;
+            Object.entries(to.params).forEach(([k, v]) => {
+                str = str.replace(`{{${k}}}`, v);
+            });
+            element.setAttribute('content', str);
+        }
+    });
+
+    if (to.meta?.title) {
+        let str = to.meta.title;
+        Object.entries(to.params).forEach(([k, v]) => {
+            str = str.replace(`{{${k}}}`, v);
+        });
+        document.querySelector("title").innerHTML = str;
+    } else {
+        document.querySelector("title").innerHTML = "DRAFTPUCK - A live hockey drinking game.";
+    }
+
+    next();
 });
 
 export default router;
--- src/DraftPuck/ClientApp/src/views/Lobby.vue
+++ src/DraftPuck/ClientApp/src/views/Lobby.vue
@@ -1,54 +1,55 @@
 ﻿<template>
 
     <div class="d-flex overflow-hidden flex-column" style="height: 100%;">
-        <b-modal v-model="showInstructions" title="How To Play" header-class="bg-stone-900" body-class="bg-stone-100 text-stone-900" ok-only>
-            <div>
-                <ol class="ml-0 pl-3">
-                    <li>
-                        <strong class="text-uppercase">Invite Your Friends</strong>
-                        <p class="mt-1 mb-0">Have your friends join the lobby by entering your lobby code (<strong>{{ lobby.joinCode }}</strong>) at the home screen...</p>
-                        <p class="my-1 ml-2"><strong class="text-uppercase">or</strong></p>
-                        <p>...share the current URL, which will auto-populate the lobby code for them!</p>
-                    </li>
-
-                    <li class="pt-3">
-                        <strong class="text-uppercase">Pick Your Players</strong>
-                        <p class="mt-1">Once picks become available for a game, click the <strong>ROSTERS</strong> button to see a list of each team's players.</p>
-                        <p class="mt-1">Available players will have a <strong>PICK</strong> button beside their name, whereas players with a username have already been chosen.</p>
-                        <p class="mt-1">Choose carefully! Once you've made your choice, only the lobby creator can undo it for you.</p>
-                        <p class="mt-1">
-                            <strong>Playing with bots?</strong> Bots can make their picks as soon as rosters become available. The lobby creator can initiate the bot
-                            picks by opening the <strong>ROSTERS</strong> and clicking the <strong>MAKE BOT PICKS</strong> button.
-                        </p>
-                    </li>
-
-                    <li class="pt-3">
-                        <strong class="text-uppercase">Cross your fingers</strong>
-                        <p class="mt-1">
-                            If one of your players scores, you get to make someone drink! When this happens, you'll receive a notification letting you know you've got a drink available. Additionally,
-                            this information is listed next to your name in the <strong>Lobby</strong> view.
-                        </p>
-                        <p class="mt-1">
-                            Once you've earned a drink, you can dish it out by going to the <strong>Lobby</strong> view, clicking the desired recipient's name, and selecting <strong>"Give a drink!"</strong>.
-                        </p>
-                        <p class="mt-1">
-                            The recipient will receive a notification letting them know that you've sent a drink their way.
-                        </p>
-                    </li>
-
-                    <li class="pt-3">
-                        <strong class="text-uppercase">Keep an eye on "the feed"</strong>
-                        <p class="mt-1">
-                            Last but certainly not least: <strong>🚨 The Feed</strong> will be your one-stop-shop for all lobby events, from penalties and goals, to drinks earned and drinks taken.
-                        </p>
-                        <p class="mt-1">
-                            You can tailor your feed to your liking by using the <strong><i class="fi fi-sr-settings"></i> Settings</strong> icon at the top. You can also see a history of all drinks assigned by clicking the <strong><i class="fi fi-sr-list"></i> Timeline</strong> button.
-                        </p>
-                    </li>
-                </ol>
-            </div>
-        </b-modal>
         <template v-if="!isInvalidLobby">
+
+            <b-modal v-model="showInstructions" title="How To Play" header-class="bg-stone-900" body-class="bg-stone-100 text-stone-900" ok-only>
+                <div>
+                    <ol class="ml-0 pl-3">
+                        <li>
+                            <strong class="text-uppercase">Invite Your Friends</strong>
+                            <p class="mt-1 mb-0">Have your friends join the lobby by entering your lobby code (<strong>{{ lobby.joinCode }}</strong>) at the home screen...</p>
+                            <p class="my-1 ml-2"><strong class="text-uppercase">or</strong></p>
+                            <p>...share the current URL, which will auto-populate the lobby code for them!</p>
+                        </li>
+
+                        <li class="pt-3">
+                            <strong class="text-uppercase">Pick Your Players</strong>
+                            <p class="mt-1">Once picks become available for a game, click the <strong>ROSTERS</strong> button to see a list of each team's players.</p>
+                            <p class="mt-1">Available players will have a <strong>PICK</strong> button beside their name, whereas players with a username have already been chosen.</p>
+                            <p class="mt-1">Choose carefully! Once you've made your choice, only the lobby creator can undo it for you.</p>
+                            <p class="mt-1">
+                                <strong>Playing with bots?</strong> Bots can make their picks as soon as rosters become available. The lobby creator can initiate the bot
+                                picks by opening the <strong>ROSTERS</strong> and clicking the <strong>MAKE BOT PICKS</strong> button.
+                            </p>
+                        </li>
+
+                        <li class="pt-3">
+                            <strong class="text-uppercase">Cross your fingers</strong>
+                            <p class="mt-1">
+                                If one of your players scores, you get to make someone drink! When this happens, you'll receive a notification letting you know you've got a drink available. Additionally,
+                                this information is listed next to your name in the <strong>Lobby</strong> view.
+                            </p>
+                            <p class="mt-1">
+                                Once you've earned a drink, you can dish it out by going to the <strong>Lobby</strong> view, clicking the desired recipient's name, and selecting <strong>"Give a drink!"</strong>.
+                            </p>
+                            <p class="mt-1">
+                                The recipient will receive a notification letting them know that you've sent a drink their way.
+                            </p>
+                        </li>
+
+                        <li class="pt-3">
+                            <strong class="text-uppercase">Keep an eye on "the feed"</strong>
+                            <p class="mt-1">
+                                Last but certainly not least: <strong>🚨 The Feed</strong> will be your one-stop-shop for all lobby events, from penalties and goals, to drinks earned and drinks taken.
+                            </p>
+                            <p class="mt-1">
+                                You can tailor your feed to your liking by using the <strong><i class="fi fi-sr-settings"></i> Settings</strong> icon at the top. You can also see a history of all drinks assigned by clicking the <strong><i class="fi fi-sr-list"></i> Timeline</strong> button.
+                            </p>
+                        </li>
+                    </ol>
+                </div>
+            </b-modal>
 
             <div class="bg-stone-900 px-sm-4 px-2 py-2 shadow position-relative d-flex align-items-center" style="z-index: 10;">
                 <router-link to="/" class="banner-logo text-stone-0 text-decoration-none" style="cursor: pointer">
@@ -200,8 +201,8 @@
             try {
                 this.isLoading = true;
                 await this.getLobby(this.joinCode);
-                this.lastLobbyUpdate = new Date();
 
+                this.lastLobbyUpdate = new Date();
                 this.selectedDate = format(addHours(this.lobby.created, -4), 'yyyy-MM-dd');
 
                 const currentLobbyMember = this.lobby.members.find(m => m.userId === this.currentUserId);
