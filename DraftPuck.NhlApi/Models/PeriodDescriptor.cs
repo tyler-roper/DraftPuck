b00309d154a4c6c015a289f5b56d53f9eff23d94
tyler.roper <tyroper22@gmail.com>
2024-01-10 19:11:16 -0500

    Remove extra NHL project

--- DraftPuck.NhlApi/Models/PeriodDescriptor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DraftPuck.NhlApi.Models
-{
-    public class PeriodDescriptor
-    {
-        public int Number { get; set; }
-        public string PeriodType { get; set; } = null!;
-    }
-}
