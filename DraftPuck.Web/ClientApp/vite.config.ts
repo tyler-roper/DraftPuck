75f4b8fb8230b4fc7f6a5bde1ad08f0763298c01
tyler.roper <tyroper22@gmail.com>
2025-02-07 20:19:50 -0500

    improve signalr logging

--- DraftPuck.Web/ClientApp/vite.config.ts
+++ DraftPuck.Web/ClientApp/vite.config.ts
@@ -25,10 +25,10 @@ export default defineConfig(({ mode }) => {
       host: 'localhost',
       port: 17010,
       watch: { usePolling: false },
-      https: {
-        pfx: resolve(process.env.USERPROFILE!, '.aspnet/https/DraftPuck.pfx'),
-        passphrase: 'b30b3f16-18c1-4a00-8f8f-0ce0fba61cb2'
-      },
+      // https: {
+      //   pfx: resolve(process.env.USERPROFILE!, '.aspnet/https/DraftPuck.pfx'),
+      //   passphrase: 'b30b3f16-18c1-4a00-8f8f-0ce0fba61cb2'
+      // },
       proxy: {
         '/api': {
           target: 'https://localhost:17000',
