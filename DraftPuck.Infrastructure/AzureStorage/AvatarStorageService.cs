678a940196f39a5660c6b13e80e0ab8bafff0d1c
Tyler Roper <32524387+tyler-roper@users.noreply.github.com>
2025-11-15 12:55:47 -0500

    Feature/remove unused items (#96)
    * Testing Webhooks
    
    * chore(app,infra): remove unused items
    
    - Remove unused ScheduleHandler
    - Remove unused Usings from AzureStorageService

--- DraftPuck.Infrastructure/AzureStorage/AvatarStorageService.cs
+++ DraftPuck.Infrastructure/AzureStorage/AvatarStorageService.cs
@@ -1,7 +1,5 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using System;
-using System.Reflection.Metadata;
 
 namespace DraftPuck.Infrastructure.AzureStorage;
 public class AvatarStorageService(BlobContainerClient container) : IAvatarStorageService
